5da153cba922cce08ccd422f006498259efca277
unknown <li_yang@microad-tech.com>
2014-11-22 20:00:42 +0800

    1.modify NewsReader   2.delete animation test1 and test2

--- NewsReader/src/com/example/newsreader/MainActivity.java
+++ NewsReader/src/com/example/newsreader/MainActivity.java
@@ -1,26 +1,53 @@
 package com.example.newsreader;
 
-import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+
+	boolean mIsDualPane;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// 是否为多面板显示模式
+		View articleView = findViewById(R.id.article);
+        mIsDualPane = articleView != null && 
+                        articleView.getVisibility() == View.VISIBLE;
+        
+        Button btn = (Button) findViewById(R.id.button1);
+        OnClickListener ocl = new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+            	if(mIsDualPane){
+            		// 改变右侧面板显示内容
+                	TextView tv = (TextView) findViewById(R.id.articleText);
+                	tv.setText("changed by headLines");
+                }else{
+                	// 打开新窗口
+                	Intent intent = new Intent(getApplicationContext(), ArticleActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        
+        btn.setOnClickListener(ocl);
 	}
-	
-	public void showArticle(View v){
-		Log.i("Headlines", "showArticle");
-		Intent intent = new Intent(this, ArticleActivity.class);
-        startActivity(intent);
-	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
