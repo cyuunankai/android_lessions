fbd080545fdcd7c74508315ea655ff089013d07f
li_yang <li_yang@microad-tech.com>
2014-11-28 16:29:42 +0800

    add Lateral navigation

---
+++ LateralNavigation/src/com/example/lateralnavigation/CollectionDemoActivity.java
@@ -0,0 +1,186 @@
+package com.example.lateralnavigation;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.lateralnavigation.tabs.Tab1Fragment;
+import com.example.lateralnavigation.tabs.Tab2Fragment;
+import com.example.lateralnavigation.tabs.Tab3Fragment;
+import com.example.lateralnavigation.tabs.Tab4Fragment;
+import com.example.lateralnavigation.tabs.Tab5Fragment;
+
+/**
+ * 组件
+ * ViewPager
+ * ActionBar
+ * 
+ * 理解
+ * ViewPager通过FragmentStatePagerAdapter.getItem表示对应页内容
+ * 
+ * 点击tab时，
+ * ActionBar.TabListener -> onTabSelected -> ViewPager.setCurrentItem -> FragmentStatePagerAdapter.getItem被触发
+ * 
+ * 手势横向滑动时，
+ * ViewPager.SimpleOnPageChangeListener -> onPageSelected -> ActionBar.setSelectedNavigationItem -> 触发点击tab处理
+ * 
+ * 初期化时，
+ * ActionBar.setSelectedNavigationItem -> 触发点击tab处理
+ *
+ */
+public class CollectionDemoActivity extends ActionBarActivity {
+
+	private MyFragmentStatePagerAdapter mMyFragmentStatePagerAdapter;
+	ViewPager mViewPager;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_collection_demo);
+
+		mMyFragmentStatePagerAdapter = new MyFragmentStatePagerAdapter(getSupportFragmentManager());
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		
+		// 设定适配器
+		mViewPager.setAdapter(mMyFragmentStatePagerAdapter);
+
+		// 添加tab
+		// 设定tab选中监听器
+		final ActionBar bar = getActionBar();
+		addTabAndSetTabListener(bar);
+
+		// 初始化手势横向滑动监听器
+		initViewPagerPageChangeListener(bar);
+
+		if (savedInstanceState != null) {
+			// 初始化选中第一个tab
+			bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
+		}
+
+	}
+	
+	/**
+	 * 添加tab
+	 * 设定tab选中监听器
+	 */
+	private void addTabAndSetTabListener(ActionBar bar){
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+		
+		ActionBar.TabListener tabListener = getTabListener();
+		bar.addTab(bar.newTab().setText("tab1111111").setTabListener(tabListener));
+		bar.addTab(bar.newTab().setText("tab2222222").setTabListener(tabListener));
+		bar.addTab(bar.newTab().setText("tab3333333").setTabListener(tabListener));
+		bar.addTab(bar.newTab().setText("tab4444444").setTabListener(tabListener));
+		bar.addTab(bar.newTab().setText("tab5555555").setTabListener(tabListener));
+	}
+	
+	/**
+	 * 点击tab时
+	 * ActionBar.TabListener -> onTabSelected -> ViewPager.setCurrentItem -> FragmentStatePagerAdapter.getItem被触发
+	 */
+	private ActionBar.TabListener getTabListener() {
+		// Create a tab listener that is called when the user changes tabs.
+		ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+
+			@Override
+			public void onTabReselected(Tab arg0,
+					android.app.FragmentTransaction arg1) {
+
+			}
+
+			@Override
+			public void onTabSelected(Tab tab,
+					android.app.FragmentTransaction arg1) {
+				
+				mViewPager.setCurrentItem(tab.getPosition());
+			}
+
+			@Override
+			public void onTabUnselected(Tab arg0,
+					android.app.FragmentTransaction arg1) {
+
+			}
+
+		};
+		
+		return tabListener;
+	}
+	
+	/**
+	 * 初始化手势横向滑动监听器
+	 */
+	private void initViewPagerPageChangeListener(final ActionBar bar) {
+		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+			@Override
+			public void onPageSelected(int position) {
+				// When swiping between pages, select the
+				// corresponding tab.
+				bar.setSelectedNavigationItem(position);
+			}
+		});
+	}
+
+	// Since this is an object collection, use a FragmentStatePagerAdapter,
+	// and NOT a FragmentPagerAdapter.
+	public class MyFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
+
+		public MyFragmentStatePagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int i) {
+			
+			switch (i) {
+			case 0:
+				return new Tab1Fragment();
+			case 1:
+				return new Tab2Fragment();
+			case 2:
+				return new Tab3Fragment();
+			case 3:
+				return new Tab4Fragment();
+			case 4:
+				return new Tab5Fragment();
+
+			}
+
+			return null;
+		}
+
+		@Override
+		public int getCount() {
+			// tab数量
+			return 5;
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.collection_demo, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
---
+++ LateralNavigation/src/com/example/lateralnavigation/tabs/Tab1Fragment.java
@@ -0,0 +1,20 @@
+package com.example.lateralnavigation.tabs;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lateralnavigation.R;
+
+public class Tab1Fragment extends Fragment {
+
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_tab1, container, false);
+    }
+
+}
